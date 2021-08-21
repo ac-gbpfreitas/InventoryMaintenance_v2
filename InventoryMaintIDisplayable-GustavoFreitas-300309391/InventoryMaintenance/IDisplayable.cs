e1d2049060570bbc5d6eaf6d54a68755f768163b
Gustavo Freitas <70547481+ac-gbpfreitas@users.noreply.github.com>
2021-08-21 11:43:17 -0700

    InventoryMaintenance_v2
    Creating, Reading, and writing a txt file

---
+++ InventoryMaintIDisplayable-GustavoFreitas-300309391/InventoryMaintenance/IDisplayable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryMaintenance
+{
+    interface IDisplayable {
+
+        public string GetDisplayText();
+    }
+}
